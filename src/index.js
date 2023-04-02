91e71e56ce666e447f4af42e089a3658312356dc
unknown <moekhalil70@gmail.com>
2023-04-02 16:57:36 +0300

    Retrieved Members with Admin Role

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { config } from "dotenv";
 import { Client, GatewayIntentBits, Events } from "discord.js";
 import { REST } from "@discordjs/rest";
+import { db } from "./db/db.js";
 config();
 
 const client = new Client({
@@ -18,6 +19,15 @@ client.login(process.env.TOKEN);
 
 client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
+  const guild = client.guilds.cache.get(process.env.GUILD_ID);
+  const members = guild.members.cache;
+  const roles = guild.roles.cache;
+  console.log(`Members: ${members.size} | Roles: ${roles.size}`);
+  members.forEach((member) => {
+    member._roles.includes("1092068511515422862") &&
+      db.data.posts.push({ id: member.id, name: member.user.username });
+  });
+  db.write();
 });
 
 client.on(Events.MessageCreate, (message) => {
